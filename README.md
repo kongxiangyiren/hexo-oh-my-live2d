53ae078813f58fa1ae7db9b9c9b408e0edcd58bb
空巷一人 <1530688385@qq.com>
2023-01-27 16:49:43 +0800

    第一次发布

---
+++ README.md
@@ -0,0 +1,30 @@
+一个用于 hexo 的 live2d 看板娘插件，支持所有版本的模型。
+\_config.yml
+
+```yaml
+OhMyLive2d:
+  enable: true
+  CDN: https://npm.elemecdn.com/oh-my-live2d/dist/index.min.js
+#  非全部配置 详情请看 https://oh-my-live2d.netlify.app/
+ option:
+    source: 'https://npm.elemecdn.com'
+    sayHello: false
+    transitionTime: 1000
+    models:
+      scale: 1.2
+      path: /live2d-widget-model-koharu@1.0.5/assets/koharu.model.json
+      x: 0
+      'y': 0
+      stageStyle:
+        backgroundColor: 'rgba(0, 0, 0, 0)'
+        width: auto
+        height: auto
+    tips:
+      style:
+        width: 230
+        height: 120
+        offsetX: 0
+        offsetY: 90
+      idleTips:
+        interval: 15000
+```
---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const urlFor = require('hexo-util').url_for.bind(hexo);
+
+hexo.extend.filter.register('after_generate', function () {
+  // 首先获取整体的配置项名称
+  const config = hexo.config.OhMyLive2d;
+  // 如果配置开启
+  if (!(config && config.enable)) return;
+  // 集体声明配置项
+  const data = {
+    CDN: config.CDN
+      ? urlFor(config.CDN)
+      : 'https://npm.elemecdn.com/oh-my-live2d/dist/index.min.js',
+    option: config.option ? JSON.stringify(config.option) : undefined
+  };
+
+  //cdn资源声明
+  //脚本资源
+  const js_text = `<script data-pjax src="${data.CDN}"></script>`;
+  //挂载容器脚本
+  var user_info_js = `<script data-pjax>
+  OML2D.loadLive2DModel(${data.option});
+  </script>`;
+  // 注入用户脚本
+  // 注入样式资源
+  hexo.extend.injector.register('body_end', js_text, 'default');
+  // 此处利用挂载容器实现了二级注入
+  hexo.extend.injector.register('body_end', user_info_js, 'default');
+});
